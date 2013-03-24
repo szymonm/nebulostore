8122f2c52c255b9bee46bc53e0eb84edda115c5f
Bolesław Kulbabiński <bk277531@students.mimuw.edu.pl>
2013-03-24 17:16:59 +0000

    Making TestingPeer handle both test servers and clients.

--- scripts/generate-config-files.sh
+++ scripts/generate-config-files.sh
@@ -10,8 +10,7 @@ PEER_NUM=3
 TEST_ITER=3
 BOOTSTRAP_ADDRESS="localhost"
 
-PEERNAME="org.nebulostore.appcore.Peer"
-TESTPEERNAME="org.nebulostore.systest.TestingPeer"
+PEERNAME="org.nebulostore.systest.TestingPeer"
 
 COMMON_ARGS="--BOOTSTRAP_PORT=10201 --BOOTSTRAP_TOMP2P_PORT=10301"
 
@@ -48,14 +47,14 @@ do
     # Make last peer test server.
     if [ $i -eq $PEER_NUM ]
     then
-        CLASS_NAME=$TESTPEERNAME
+        IS_SERVER="true"
     else
-        CLASS_NAME=$PEERNAME
+        IS_SERVER="false"
     fi
 
     padded=`printf "%02d" $i`
-    ./resources/conf/generate_config.py $COMMON_ARGS --APP_KEY=$i$i --CLASS_NAME=$CLASS_NAME --TEST_LIST=$CONCAT\
+    ./resources/conf/generate_config.py $COMMON_ARGS --APP_KEY=$i$i --CLASS_NAME=$PEERNAME --TEST_LIST=$CONCAT\
          --BOOTSTRAP_MODE=$BOOTSTRAP_MODE --CLI_PORT=101$padded --TOMP2P_PORT=103$padded --BDB_TYPE=$BDB_TYPE\
-         --BOOTSTRAP_ADDRESS=$BOOTSTRAP_ADDRESS < ./resources/conf/Peer.xml.template > Peer.xml.$i
+         --BOOTSTRAP_ADDRESS=$BOOTSTRAP_ADDRESS --IS_SERVER=$IS_SERVER < ./resources/conf/Peer.xml.template > Peer.xml.$i
 done
 
