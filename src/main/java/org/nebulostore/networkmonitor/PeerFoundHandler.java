932ca45144ab49d76efe7847a438424f74049321
szymon <szymon@Grubas>
2013-08-27 12:47:11 +0200

    missing file?
    Change-Id: I858c4351176ea1d44492b1cd2e491c8abac1ebf2

---
+++ src/main/java/org/nebulostore/networkmonitor/PeerFoundHandler.java
@@ -0,0 +1,37 @@
+package org.nebulostore.broker;
+
+import org.nebulostore.appcore.exceptions.NebuloException;
+import org.nebulostore.appcore.messaging.Message;
+import org.nebulostore.appcore.messaging.MessageVisitor;
+import org.nebulostore.appcore.modules.JobModule;
+import org.nebulostore.communication.messages.CommPeerFoundMessage;
+import org.nebulostore.networkmonitor.NetworkContext;
+
+/**
+ * Module handle CommPeerFoundMessage.
+ * @author szymonmatejczyk
+ */
+public class PeerFoundHandler extends JobModule {
+
+  private final PeerFoundHandlerVisitor visitor_ = new PeerFoundHandlerVisitor();
+
+  @Override
+  protected void processMessage(Message message) throws NebuloException {
+    message.accept(visitor_);
+  }
+
+  /**
+   * Visitor.
+   * @author szymonmatejczyk
+   */
+  protected class PeerFoundHandlerVisitor extends MessageVisitor<Void> {
+    public Void visit(CommPeerFoundMessage message) {
+      jobId_ = message.getId();
+      NetworkContext context = NetworkContext.getInstance();
+      context.addFoundPeer(message.getSourceAddress());
+      endJobModule();
+      return null;
+    }
+  }
+
+}
